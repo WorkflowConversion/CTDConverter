a5c8248e21d20a7a6838359684048fe3f379811d
Matthias Bernt <m.bernt@ufz.de>
2021-02-22 12:22:08 +0100

    limit line length

--- create_galaxy_tests.sh
+++ create_galaxy_tests.sh
@@ -1,3 +1,7 @@
-conda create -y --quiet --override-channels --channel iuc --channel conda-forge --channel bioconda --channel defaults --name ctdopts-1.4 ctdopts=1.4 lxml
+conda create -y --quiet\
+    --override-channels -c iuc -c conda-forge -c bioconda -c defaults\
+    --name ctdopts-1.4 ctdopts=1.4 lxml
 
-python convert.py galaxy -i tests/test-data/*.ctd -o tests/test-data/ -m tests/test-data/macros.xml -f tests/test-data/filetypes.txt --test-test -p tests/test-data/hardcoded_params.json --tool-version 3.8
+CTDConverter galaxy -i tests/test-data/*.ctd -o tests/test-data/\
+    -m tests/test-data/macros.xml -f tests/test-data/filetypes.txt\
+    --test-test -p tests/test-data/hardcoded_params.json --tool-version 3.8
